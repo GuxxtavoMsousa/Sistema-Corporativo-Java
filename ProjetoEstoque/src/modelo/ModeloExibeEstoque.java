ea4f4a5127e2fa405fb50717bdd47611363d1090
Gustavo Moreira <122908233+GuxxtavoMsousa@users.noreply.github.com>
2023-05-06 03:45:17 -0300

    Add files via upload

---
+++ ProjetoEstoque/src/modelo/ModeloExibeEstoque.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class ModeloExibeEstoque {
+    String codigo;
+    String nome;
+    String quantidade;
+    String custo;
+    String preçoVenda;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCusto() {
+        return custo;
+    }
+
+    public void setCusto(String custo) {
+        this.custo = custo;
+    }
+
+    public String getPreçoVenda() {
+        return preçoVenda;
+    }
+
+    public void setPreçoVenda(String preçoVenda) {
+        this.preçoVenda = preçoVenda;
+    }
+    
+    
+}
